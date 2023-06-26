0231b615deed566d08e43226f589c637c564536f
Lianny Coringa <lianny_costa@hotmail.com>
2023-06-26 16:29:07 -0300

    Requisito 4 feito

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import App from "../App";
+import renderWithRouter from "../renderWithRouter";
+import { screen } from "@testing-library/react";
+
+test('Testando a pagina NotFound quando coloca uma rota inexistente', () => {
+  renderWithRouter(<App />, {route: './NotFound'});
+
+  const h2 = screen.getByRole('heading', {name: 'Page requested not found'});
+  const img = screen.getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(h2).toBeInTheDocument();
+  expect(img).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
