cc6f0935d14a9599f8b597b0621809a1f2442e5d
Lianny Coringa <lianny_costa@hotmail.com>
2023-06-26 18:59:16 -0300

    Tirando espacos

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -11,42 +11,34 @@ test('Testando Pokedex', async () => {
   const buttonNext = screen.getByRole('button', { name: 'Próximo Pokémon' });
   const img = screen.getByAltText('Pikachu sprite');
   expect(img).toHaveProperty('src', 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png');
-
   // Segundo
   await user.click(buttonNext);
   const img1 = screen.getByAltText(pokemonList[1].name + ' sprite');
   expect(img1).toHaveProperty('src', pokemonList[1].image);
-
   //Terceiro
   await user.click(buttonNext);
   const img2 = screen.getByAltText(pokemonList[2].name + ' sprite');
   expect(img2).toHaveProperty('src', pokemonList[2].image);
-
   //Quarto
   await user.click(buttonNext);
   const img3 = screen.getByAltText(pokemonList[3].name + ' sprite');
   expect(img3).toHaveProperty('src', pokemonList[3].image);
-
   //Quinto
   await user.click(buttonNext);
   const img4 = screen.getByAltText(pokemonList[4].name + ' sprite');
   expect(img4).toHaveProperty('src', pokemonList[4].image);
-
   //Sexto
   await user.click(buttonNext);
   const img5 = screen.getByAltText(pokemonList[5].name + ' sprite');
   expect(img5).toHaveProperty('src', pokemonList[5].image);
-
   //Setimo
   await user.click(buttonNext);
   const img6 = screen.getByAltText(pokemonList[6].name + ' sprite');
   expect(img6).toHaveProperty('src', pokemonList[6].image);
-
   //Oitavo
   await user.click(buttonNext);
   const img7 = screen.getByAltText(pokemonList[7].name + ' sprite');
   expect(img7).toHaveProperty('src', pokemonList[7].image);
-
   //Nono
   await user.click(buttonNext);
   const img8 = screen.getByAltText(pokemonList[8].name + ' sprite');
