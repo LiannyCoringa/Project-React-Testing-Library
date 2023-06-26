ce46a475e8235475e2243ca5c07bf90af30e12ec
Lianny Coringa <lianny_costa@hotmail.com>
2023-06-26 16:03:06 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,22 @@
-test('', () => {});
+import App from "../App";
+import renderWithRouter from "../renderWithRouter";
+import { screen } from "@testing-library/react";
+
+test('Testa se nao tem pokemon favoritado', () => {
+  renderWithRouter(<App />, {route: '/favorites'});
+
+  const p = screen.getByText('No favorite Pokémon found');
+  expect(p).toBeInTheDocument();
+});
+
+test('Testa se so sao exibidos os favoritos', async () => {
+  const { user } = renderWithRouter(<App />, {route: '/pokemon/25'});
+// screen.getByRole('checkbox')
+  const input = screen.getByLabelText('Pokémon favoritado?');
+  const linkFavorite = screen.getByRole('link', {name: 'Favorite Pokémon'});
+  await user.click(input);
+  await user.click(linkFavorite);
+  expect(screen.getByRole('heading', {name: 'Favorite Pokémon'})).toBeInTheDocument();
+  const img = screen.getByAltText('Pikachu is marked as favorite');
+  expect(img).toHaveAttribute('src', '/star-icon.svg');
+})
