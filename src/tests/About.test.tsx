8e1c3bdaeb248947e1d2ab0a71a11741b7272a43
Lianny Coringa <lianny_costa@hotmail.com>
2023-06-26 15:36:06 -0300

    Requisito 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import App from "../App";
+import renderWithRouter from "../renderWithRouter";
+import { screen } from "@testing-library/react";
+
+test('Testando a pagina About', () => {
+  renderWithRouter(<App />, {route: '/about'});
+
+  const h2 = screen.getByRole('heading', {name: 'About Pokédex'});
+  const pOne = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon');
+  const pTwo = screen.getByText('One can filter Pokémon by type, and see more details for each one of them');
+  const img = screen.getByAltText('Pokédex');
+
+  expect(h2).toBeInTheDocument();
+  expect(pOne).toBeInTheDocument();
+  expect(pTwo).toBeInTheDocument();
+  expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
